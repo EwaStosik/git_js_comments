2dfa65a1eead6a784aeebb534c260f7257f79a57
Ewa Stosik <e.stosik@outlook.com>
2017-11-11 22:58:45 +0100

    adding file

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var name = prompt('Enter your name');
+//Test komentarzy 
+alert('Hello, ' + name);
+//tutaj jeszcze jeden komentarz
+console.log('Hello, ' + name);
+//i ostatni komentarz jeszcze tutaj
